d0b7df4baef4078e4e38fae38eb95e3ba4e49f3c
Stanisalv Vakhno <stanislav.vakhno@synebo.io>
2022-01-18 21:51:39 +0200

    Adding create new tweet block

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,15 @@
-import { Grid, Typography, makeStyles, Container, InputBase, Paper } from '@material-ui/core'
+import { TextField, IconButton, ListItem, Divider, Avatar, ListItemText, List, TextareaAutosize, CircularProgress, Grid, Typography, makeStyles, Container, InputBase, Paper, Button, } from '@material-ui/core'
 import React from 'react'
+
+import classNames from 'classnames';
 
 import { createStyles, withStyles } from '@material-ui/styles';
 import { grey } from '@material-ui/core/colors';
 import { Tweet } from '../components/Tweet/Tweet'
 import { SideMenu } from '../components/SideMenu/SideMenu';
+import EmojiIcon from '@material-ui/icons/SentimentSatisfiedAltOutlined';
+import ImageIcon from '@material-ui/icons/ImageOutlined';
+import theme from '../theme';
 
 export const homeStyles = makeStyles((theme) => ({
   wrapper: {
@@ -112,19 +117,82 @@ export const homeStyles = makeStyles((theme) => ({
     paddingTop: 20,
     position: 'sticky',
     top: 0,
-  }
+  },
+  addForm: {
+    padding: 20,
+  },
+  addFormBody: {
+    display: 'flex',
+    width: '100%',
+  },
+  addFormBottom: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  addFormBottomActions: {
+    marginTop: 10,
+    paddingLeft: 70,
+  },
+  addFormTextarea: {
+    width: '100%',
+    border: 0,
+    fontSize: 20,
+    outline: 'none',
+    fontFamily: 'inherit',
+    resize: 'none',
+  },
+  addFormBottomLine: {
+    height: 12,
+    backgroundColor: '#E6ECF0',
+  },
+  addFormCircleProgress: {
+    position: 'relative',
+    width: 20,
+    height: 20,
+    margin: '0 10px',
+    '& .MuiCircularProgress-root': {
+      position: 'absolute',
+    },
+  },
+  addFormBottomRight: {
+    display: 'flex',
+    alignItems: 'center',
+  },
 }))
 
-const SearchTextField = withStyles(() => 
-  createStyles({
-    input: {
+const SearchTextField = withStyles((theme) => ({
+  root: {
+    '& .MuiOutlinedInput-root': {
       borderRadius: 30,
       backgroundColor: '#E6ECF0',
-      height: 45,
       padding: 0,
-    }
-  })
-)(InputBase);
+      paddingLeft: 15,
+      '& .Mui-focused': {
+        backgroundColor: '#fff',
+        '& fieldset': { 
+          borderWidth: 1, borderColor: theme.palette.primary.main 
+        },
+        '& svg path': {
+          fill: theme.palette.primary.main,
+        },
+      },
+      '&:hover': {
+        '& fieldset': { 
+          borderColor: 'transparent' 
+        },
+      },
+      '& fieldset': {
+        borderColor: 'transparent',
+        borderWidth: 1,
+      },
+    },
+    '& .MuiOutlinedInput-input': {
+      padding: '12px 14px 14px 5px',
+    },
+  },
+}))(TextField);
+
 
 export const Home: React.FC = (): React.ReactElement => {
   const classes = homeStyles();
@@ -137,8 +205,54 @@ export const Home: React.FC = (): React.ReactElement => {
           </Grid>
           <Grid item sm={8} md={6}>
             <Paper className={classes.tweetsWrapper} variant='outlined'>
-              <Paper className={classes.tweetsWrapperHeader} variant='outlined'>
+              <Paper style={{position: 'sticky', top: 0, backgroundColor: '#fff', opacity: 1, zIndex: 100}} className={classes.tweetsWrapperHeader} variant='outlined'>
                 <Typography variant='h6'>Главная</Typography>
+              </Paper>
+              <Paper>
+                <div className={classes.addForm}>
+                  <div className={classes.addFormBody}>
+                    <Avatar
+                      className={classes.tweetAvatar}
+                      alt={'User Avatar'}
+                      src='https://media.istockphoto.com/photos/young-man-is-playing-with-a-dog-and-do-selfie-picture-id1300658241?b=1&k=20&m=1300658241&s=170667a&w=0&h=0lrTViinfnDjbWDgxV0TDDSAXvzSgmrN-pKq0q60hqA='
+                    />
+                    <TextareaAutosize
+                      className={classes.addFormTextarea}
+                      placeholder={'Tweet textarea'}
+                    />
+                  </div>
+                  <div className={classes.addFormBottom}>
+                    <div className={classNames(classes.tweetFooterIcons, classes.addFormBottomActions)}>
+                      <IconButton color='primary'>
+                        <ImageIcon style={{fontSize: 20}}/>
+                      </IconButton>
+                      <IconButton color='primary'>
+                        <EmojiIcon style={{fontSize: 20}}/>
+                      </IconButton>
+                    </div>
+                    <div className={classes.addFormBottomRight}>
+                      <span>280</span>
+                      <div className={classes.addFormCircleProgress}>
+                        <CircularProgress 
+                          variant='static' 
+                          size={20}
+                          thickness={5}
+                          value={18}/>
+                        <CircularProgress 
+                          style={{color:'rgba(0, 0, 0, 0.1)'}}
+                          variant='static'
+                          size={20}
+                          thickness={4}
+                          value={100}
+                        />
+                      </div>
+                      <Button color='primary' variant='contained'>
+                        Твитнуть
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+                <div className={classes.addFormBottomLine}/>
               </Paper>
               {
                 [...new Array(20).fill(
@@ -150,7 +264,7 @@ export const Home: React.FC = (): React.ReactElement => {
                   fullName: 'Glafira Shur',
                   userName: 'GlafiraShur',
                   tweetTime: '1 ч.',
-                  avatarUrl: 'https://media.istockphoto.com/photos/excited-woman-wearing-rainbow-cardigan-picture-id1327495437?k=20&m=1327495437&s=612x612&w=0&h=uYgb28tIs15a-rrnli1gqAiG9mBhazPrui9FRpqu9UM='
+                  avatarUrl: 'https://media.c.com/photos/excited-woman-wearing-rainbow-cardigan-picture-id1327495437?k=20&m=1327495437&s=612x612&w=0&h=uYgb28tIs15a-rrnli1gqAiG9mBhazPrui9FRpqu9UM='
                 }}
               />
               )]
