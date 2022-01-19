3a4f090da57c0f6c931a79eeaaf9d51510b12f03
Stanisalv Vakhno <stanislav.vakhno@synebo.io>
2022-01-19 23:24:50 +0200

    Adding logic to circular progress bar (tweet posting)

--- frontend/src/components/AddTweetForm/AddTweetForm.tsx
+++ frontend/src/components/AddTweetForm/AddTweetForm.tsx
@@ -6,15 +6,26 @@ import TextareaAutosize from '@material-ui/core/TextareaAutosize'
 import EmojiIcon from '@material-ui/icons/SentimentSatisfiedAltOutlined';
 import ImageIcon from '@material-ui/icons/ImageOutlined';
 import {homeStyles} from '../../pages/Home'
-import React from 'react'
+import {useState} from 'react'
 import classNames from 'classnames'
-
 
 interface AddTweetFormProps {
   classes: ReturnType<typeof homeStyles>;
 }
 
+const MAX_LENGTH = 280;
+
 export const AddTweetForm: React.FC<AddTweetFormProps> = ({classes}: AddTweetFormProps): React.ReactElement => {
+  const [text, setText] = useState<string>('');
+  const textLimitPercent =  Math.round((text.length / MAX_LENGTH) * 100);
+  const textCount = MAX_LENGTH - text.length;
+
+  const handleChangeText = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    if(e.currentTarget){
+      setText(e.currentTarget.value)
+    }
+  }
+  
   return (
     <div className={classes.addForm}>
     <div className={classes.addFormBody}>
@@ -24,6 +35,8 @@ export const AddTweetForm: React.FC<AddTweetFormProps> = ({classes}: AddTweetFor
         src='https://media.istockphoto.com/photos/young-man-is-playing-with-a-dog-and-do-selfie-picture-id1300658241?b=1&k=20&m=1300658241&s=170667a&w=0&h=0lrTViinfnDjbWDgxV0TDDSAXvzSgmrN-pKq0q60hqA='
       />
       <TextareaAutosize
+        onChange={handleChangeText}
+        value={text}
         className={classes.addFormTextarea}
         placeholder={'Tweet textarea'}
       />
@@ -38,22 +51,27 @@ export const AddTweetForm: React.FC<AddTweetFormProps> = ({classes}: AddTweetFor
         </IconButton>
       </div>
       <div className={classes.addFormBottomRight}>
-        <span>280</span>
-        <div className={classes.addFormCircleProgress}>
-          <CircularProgress 
-            variant='static' 
-            size={20}
-            thickness={5}
-            value={18}/>
-          <CircularProgress 
-            style={{color:'rgba(0, 0, 0, 0.1)'}}
-            variant='static'
-            size={20}
-            thickness={4}
-            value={100}
-          />
-        </div>
-        <Button color='primary' variant='contained'>
+        {text && 
+          (<>
+            <span>{textCount}</span>
+            <div className={classes.addFormCircleProgress}>
+              <CircularProgress 
+                variant='static' 
+                size={20}
+                thickness={5}
+                value={text.length >= MAX_LENGTH ? 100 : textLimitPercent}
+                style={text.length >= MAX_LENGTH ? {color: 'red'} : undefined}/>
+              <CircularProgress 
+                style={{color:'rgba(0, 0, 0, 0.1)'}}
+                variant='static'
+                size={20}
+                thickness={4}
+                value={100}
+              />
+            </div>
+          </>)
+        }
+        <Button disabled={textLimitPercent >= 100} color='primary' variant='contained'>
           Твитнуть
         </Button>
       </div>
